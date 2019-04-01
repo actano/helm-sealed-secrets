3a743380b88100b9cf86969dbe70e7de873712d4
thrucker <t.rucker@xpure.de>
2019-04-01 17:32:38 +0200

    Add basic update command

--- sealed-secrets.sh
+++ sealed-secrets.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+HELM_BIN="${HELM_BIN:-helm}"
+
 function usage() {
   cat <<EOF
 Available commands:
@@ -7,7 +9,42 @@ Available commands:
 EOF
 }
 
+function is_help() {
+  case "$1" in
+    --help|-h|help)
+      return 0
+      ;;
+    *)
+      return 1
+      ;;
+  esac
+}
+
+function update_usage() {
+  cat <<EOF
+Update sealed secrets
+
+Example:
+  $ ${HELM_BIN} sealed-secrets update <input path>
+EOF
+}
+
+function update() {
+  if is_help "$1"; then
+    update_usage
+    return
+  fi
+}
+
 case "${1:-help}" in
+  update)
+    if [[ $# -lt 1 ]]; then
+      update_usage
+      echo "Error: input directory required"
+      exit 1
+    fi
+    update "$2"
+    ;;
   --help|-h|help)
     usage
     ;;
