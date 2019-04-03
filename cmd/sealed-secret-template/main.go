1ac6ac70834832328698f0f53e1f6752968e0ea2
Hendrik Purmann <h.purmann@googlemail.com>
2019-04-03 13:59:25 +0200

    Add optional to optional program arguments

--- cmd/sealed-secret-template/main.go
+++ cmd/sealed-secret-template/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 type config struct {
-    VaultEndpoint                    string `flag:"vault-address" env:"VAULT_ADDR" default:"https://127.0.0.1:8200" description:"Vault API endpoint. Also configurable via VAULT_ADDR."`
-    VaultTokenFile                   string `flag:"vault-token-file" env:"VAULT_TOKEN_FILE" description:"The file which contains the vault token. Also configurable via VAULT_TOKEN_FILE."`
+    VaultEndpoint                    string `flag:"vault-address" env:"VAULT_ADDR" default:"https://127.0.0.1:8200" description:"Optional. Vault API endpoint. Also configurable via VAULT_ADDR."`
+    VaultTokenFile                   string `flag:"vault-token-file" env:"VAULT_TOKEN_FILE" description:"Optional. The file which contains the vault token. Also configurable via VAULT_TOKEN_FILE."`
     SealedSecretsControllerNamespace string `flag:"sealed-secrets-controller-namespace,c" description:"Sealed secret controller namespace"`
     InputFile                        string `flag:"input-file,i" description:"The input secret template which should be rendered and sealed."`
     OutputFile                       string `flag:"output-file,o" description:"The output file path where the sealed secret should be written to."`
