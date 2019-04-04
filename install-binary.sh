62776927f7bc429add06f44ea60f879d4b40b986
thrucker <t.rucker@xpure.de>
2019-04-04 15:17:55 +0200

    Add install script
    * to ensure that the sealed-secret-template binary is installed

---
+++ install-binary.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+VERSION=0.1.0
+
+if hash sealed-secret-template 2>/dev/null; then
+  echo "sealed-secret-template is already installed"
+else
+  OS=$(uname | tr '[:upper:]' '[:lower:]')
+  URL=https://github.com/actano/helm-sealed-secrets/releases/download/${VERSION}/sealed-secret-template_${OS}_amd64
+
+  curl -sL ${URL} > /usr/local/bin/sealed-secret-template
+  chmod +x /usr/local/bin/sealed-secret-template
+fi
