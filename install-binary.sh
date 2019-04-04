6c49e6ee9343c106598f1d138b52515c7c97d95b
thrucker <t.rucker@xpure.de>
2019-04-04 17:52:09 +0200

    Fail install script if download is not successful

--- install-binary.sh
+++ install-binary.sh
@@ -15,6 +15,16 @@ else
   OS=$(uname | tr '[:upper:]' '[:lower:]')
   URL=https://github.com/actano/helm-sealed-secrets/releases/download/${VERSION}/sealed-secret-template_${OS}_amd64
 
-  curl -sL ${URL} > /usr/local/bin/sealed-secret-template
+  temp_file=$(mktemp)
+  trap "rm ${temp_file}" EXIT
+
+  statuscode=$(curl -w "%{http_code}" -sL ${URL} -o ${temp_file})
+
+  if [[ ! "${statuscode}" == "200" ]]; then
+    echo "Failed to download binary"
+    exit 1
+  fi
+
+  cp ${temp_file} /usr/local/bin/sealed-secret-template
   chmod +x /usr/local/bin/sealed-secret-template
 fi
