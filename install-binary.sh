8e0a06813533bc36e9435fc8a10000277d789625
thrucker <t.rucker@xpure.de>
2019-04-04 17:27:56 +0200

    Check version before downloading binary

--- install-binary.sh
+++ install-binary.sh
@@ -4,9 +4,14 @@ set -ueo pipefail
 
 VERSION=0.1.2
 
-if hash sealed-secret-template 2>/dev/null; then
+function isAlreadyInstalled() {
+  hash sealed-secret-template 2>/dev/null && [[ $(sealed-secret-template -v | cut -d " " -f 3) == ${VERSION} ]]
+}
+
+if isAlreadyInstalled; then
   echo "sealed-secret-template is already installed"
 else
+  echo "Downloading sealed-secret-template version ${VERSION}"
   OS=$(uname | tr '[:upper:]' '[:lower:]')
   URL=https://github.com/actano/helm-sealed-secrets/releases/download/${VERSION}/sealed-secret-template_${OS}_amd64
 
